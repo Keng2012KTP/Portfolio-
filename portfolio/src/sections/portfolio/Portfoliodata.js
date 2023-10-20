b0c8f8291a7f6b620b3bc046c71c697b88f3b32d
KIKantaphong <k.kantaphong@kkumail.com>
2023-10-21 03:46:16 +0700

    feat: add basic portfolio section

---
+++ portfolio/src/sections/portfolio/Portfoliodata.js
@@ -0,0 +1,95 @@
+import Image1 from "../../assets/1.jpg";
+import Image2 from "../../assets/2.jpg";
+import Image3 from "../../assets/3.jpg";
+import Image4 from "../../assets/4.jpg";
+import Image5 from "../../assets/5.jpg";
+import Image6 from "../../assets/6.jpg";
+import Image7 from "../../assets/7.jpg";
+import Image8 from "../../assets/8.jpg";
+import Image9 from "../../assets/9.jpg";
+
+const Portfoliodata = [
+  {
+    id: 1,
+    category: "fullstack",
+    image: Image1,
+    title: "Project Title One (Fullstack)",
+    desc: "Provident similique accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam nihil, eveniet aliquid culpa officia aut!",
+    demo: "https://www.google.com",
+    github: "https://github.com",
+  },
+  {
+    id: 2,
+    category: "frontend",
+    image: Image2,
+    title: "Project Title Two (Frontend)",
+    desc: "Provident similique accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam nihil, eveniet aliquid culpa officia aut!",
+    demo: "https://www.google.com",
+    github: "https://github.com",
+  },
+  {
+    id: 3,
+    category: "backend",
+    image: Image3,
+    title: "Project Title Three (Backend)",
+    desc: "Provident similique accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam nihil, eveniet aliquid culpa officia aut!",
+    demo: "https://www.google.com",
+    github: "https://github.com",
+  },
+  {
+    id: 4,
+    category: "fullstack",
+    image: Image4,
+    title: "Project Title Four (Fullstack)",
+    desc: "Provident similique accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam nihil, eveniet aliquid culpa officia aut!",
+    demo: "https://www.google.com",
+    github: "https://github.com",
+  },
+  {
+    id: 5,
+    category: "fullstack",
+    image: Image5,
+    title: "Project Title Five (Fullstack)",
+    desc: "Provident similique accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam nihil, eveniet aliquid culpa officia aut! ",
+    demo: "https://www.google.com",
+    github: "https://github.com",
+  },
+  {
+    id: 6,
+    category: "frontend",
+    image: Image6,
+    title: "Project Title Six (Frontend)",
+    desc: "Provident similique accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam nihil, eveniet aliquid culpa officia aut!",
+    demo: "https://www.google.com",
+    github: "https://github.com",
+  },
+  {
+    id: 7,
+    category: "frontend",
+    image: Image7,
+    title: "Project Title Seven (Frontend)",
+    desc: "Provident similique accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam nihil, eveniet aliquid culpa officia aut!",
+    demo: "https://www.google.com",
+    github: "https://github.com",
+  },
+  {
+    id: 8,
+    category: "backend",
+    image: Image8,
+    title: "Project Title Eight (Backend)",
+    desc: "Provident similique accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam nihil, eveniet aliquid culpa officia aut!",
+    demo: "https://www.google.com",
+    github: "https://github.com",
+  },
+  {
+    id: 9,
+    category: "frontend",
+    image: Image9,
+    title: "Project Title Nine (Frontend)",
+    desc: "Provident similique accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam nihil, eveniet aliquid culpa officia aut!",
+    demo: "https://www.google.com",
+    github: "https://github.com",
+  },
+];
+
+export default Portfoliodata;
