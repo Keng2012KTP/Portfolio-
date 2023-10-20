addd70cb3c914ef833bdba75f56a08f704b1672a
KIKantaphong <k.kantaphong@kkumail.com>
2023-10-20 17:49:50 +0700

    feat:add basic header

---
+++ portfolio/src/sections/header/headerSNS.js
@@ -0,0 +1,13 @@
+import { AiOutlineInstagram } from "react-icons/ai";
+import { AiOutlineTwitter } from "react-icons/ai";
+import { AiOutlineDribbble } from "react-icons/ai";
+import { AiFillGithub } from "react-icons/ai";
+
+const headerSNS = [
+  { id: 1, icon: <AiOutlineInstagram />, link: "https://instagram.com" },
+  { id: 2, icon: <AiOutlineTwitter />, link: "https://twitter.com" },
+  { id: 3, icon: <AiOutlineDribbble />, link: "https://dribbble.com" },
+  { id: 4, icon: <AiFillGithub />, link: "https://github.com" },
+];
+
+export default headerSNS;
